28c0655b613355cd2c58a61f9d74abd7c1a0cc39
Vlad Flore <vlad.flore@virtual7.ro>
2019-03-08 12:09:54 +0100

    renamed test

--- src/test/java/com/example/demo/DemoApplicationTest.java
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class DemoApplicationTest {
 
     @Test
     public void contextLoads() {
